cf0304202b429225deb80e2a09afc7fb7fe1811d
Theo Sanderson <theo@sndrsn.co.uk>
2022-04-24 20:49:22 +0000

    try

--- upload.sh
+++ upload.sh
@@ -6,6 +6,6 @@ wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.metadat
 wget -q https://raw.githubusercontent.com/theosanderson/taxonium/master/taxoniumtools/test_data/hu1.gb
 wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.all.masked.pb.gz
 
-pip3 install taxoniumtools
+sudo pip3 install taxoniumtools
 usher_to_taxonium --input public-latest.all.masked.pb.gz --output ./public.jsonl.gz --metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage
 s3cmd put public.jsonl.gz s3://cov2tree/latest_public.jsonl.gz
