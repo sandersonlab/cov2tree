af6628dc34fbf7e72c53d1f6178e95ff88cb83d0
Theo Sanderson <theo@sndrsn.co.uk>
2022-04-24 22:38:48 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,7 +1,8 @@
 
 mkdir working
 cd working
-sed  's/MYSECRETKEY/'$VLTR_KEY'/g' ../s3cfg > s3cfg
+sed  's/MYSECRETKEY/'$DO_KEY'/g' ../s3cfg > s3cfg
+s3cmd put -P ../README.md -c s3cfg s3://cov2tree/README.md
 wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.metadata.tsv.gz
 wget -q https://raw.githubusercontent.com/theosanderson/taxonium/master/taxoniumtools/test_data/hu1.gb
 wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.all.masked.pb.gz
