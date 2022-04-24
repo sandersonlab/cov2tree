a673915c521c936e878901cca43ae8b7e0f57b4e
Theo Sanderson <theo@sndrsn.co.uk>
2022-04-24 20:40:17 +0000

    Updates

--- upload.sh
+++ upload.sh
@@ -1,3 +1,12 @@
 
 # replace s3cfg MYSECRETKEY with this above
-sed -i 's/MYSECRETKEY/'$VLTR_KEY'/g' s3cfg
+mkdir working
+cd working
+sed  's/MYSECRETKEY/'$VLTR_KEY'/g' ../s3cfg > s3cfg
+wget https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.metadata.tsv.gz
+wget https://raw.githubusercontent.com/theosanderson/taxonium/master/taxoniumtools/test_data/hu1.gb
+wget https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.all.masked.pb.gz
+
+pip install taxoniumtools
+usher_to_taxonium --input public-latest.all.masked.pb.gz --output ./public.jsonl.gz --metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage
+s3cmd put public.jsonl.gz s3://cov2tree/latest_public.jsonl.gz
