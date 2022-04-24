0a84bb0688a4b4a629b42da56372fe3d372e19ad
Theo Sanderson <theo@sndrsn.co.uk>
2022-04-24 20:45:07 +0000

    uppda

--- upload.sh
+++ upload.sh
@@ -3,9 +3,9 @@ sudo apt install -y s3cmd
 mkdir working
 cd working
 sed  's/MYSECRETKEY/'$VLTR_KEY'/g' ../s3cfg > s3cfg
-wget https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.metadata.tsv.gz
-wget https://raw.githubusercontent.com/theosanderson/taxonium/master/taxoniumtools/test_data/hu1.gb
-wget https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.all.masked.pb.gz
+wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.metadata.tsv.gz
+wget -q https://raw.githubusercontent.com/theosanderson/taxonium/master/taxoniumtools/test_data/hu1.gb
+wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.all.masked.pb.gz
 
 pip3 install taxoniumtools
 usher_to_taxonium --input public-latest.all.masked.pb.gz --output ./public.jsonl.gz --metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage
