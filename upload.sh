5984fc4773b07459460aeaace87c8090d8ecceed
Theo Sanderson <theo@sndrsn.co.uk>
2022-04-27 16:55:17 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -10,7 +10,7 @@ wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.all.mas
 wget -q https://raw.githubusercontent.com/theosanderson/treeShears/master/treeshears.py
 
 gunzip public-latest.all.masked.pb.gz
-python treeshears.py -i public-latest.all.masked.pb -o public-latest.all.masked.sheared.pb
+python3 treeshears.py -i public-latest.all.masked.pb -o public-latest.all.masked.sheared.pb
 
 sudo pip3 install taxoniumtools chronumental
 usher_to_taxonium --input public-latest.all.masked.sheared.pb --output ./public.jsonl.gz --metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage --chronumental --chronumental_steps=300
