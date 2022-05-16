1affbc98f1f39b5198559cc6d4a40c34ab86be55
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-16 20:19:58 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -15,5 +15,8 @@ gunzip public-latest.all.masked.pb.gz
 python3 treeshears.py -i public-latest.all.masked.pb -o public-latest.all.masked.sheared.pb
 
 
-usher_to_taxonium --input public-latest.all.masked.sheared.pb --output ./public.jsonl.gz --metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage --chronumental --chronumental_steps=300 --chronumental_reference_node="Wuhan-Hu-1|MN908947.3|2019-12" --overlay_html ../ack.html --title Cov2Tree
+usher_to_taxonium --input public-latest.all.masked.sheared.pb --output ./public.jsonl.gz \
+--metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage \
+--chronumental --chronumental_steps=300 --chronumental_reference_node="Wuhan-Hu-1|MN908947.3|2019-12" \
+--overlay_html ../ack.html --title Cov2Tree --remove_after_pipe
 s3cmd put -P public.jsonl.gz -c s3cfg s3://cov2tree/latest_public.jsonl.gz 
