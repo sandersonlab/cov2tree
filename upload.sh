9941ecd9ef56b82252c4002c03581b31b7d15da7
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-19 23:12:42 +0100

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -18,7 +18,8 @@ python3 treeshears.py -i public-latest.all.masked.pb -o public-latest.all.masked
 usher_to_taxonium --input public-latest.all.masked.sheared.pb --output ./public.jsonl.gz \
 --metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage \
 --chronumental --chronumental_steps=300 --chronumental_reference_node="Wuhan-Hu-1|MN908947.3|2019-12" \
---overlay_html ../ack.html --title Cov2Tree --remove_after_pipe --chronumental_date_output=chron_dates.tsv --chronumental_tree_output=chron_tree.nwk
+--overlay_html ../ack.html --title Cov2Tree --remove_after_pipe --chronumental_date_output=chron_dates.tsv --chronumental_tree_output=chron_tree.nwk \
+--clade_types=nextstrain,pango
 
 gzip chron_dates.tsv
 gzip chron_tree.nwk
