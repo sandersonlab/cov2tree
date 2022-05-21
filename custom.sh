df6a2df48dda29ed98eb7ffa19c5b01b416ccc21
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-21 13:26:52 +0100

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -12,8 +12,6 @@ wget -q https://raw.githubusercontent.com/theosanderson/treeShears/master/treesh
 sudo pip3 install taxoniumtools chronumental
 
 gunzip public-latest.all.masked.pb.gz
-python3 treeshears.py -i public-latest.all.masked.pb -o public-latest.all.masked.sheared.pb -T 5000
-
 
 usher_to_taxonium --input public-latest.all.masked.sheared.pb --output ./public.jsonl.gz \
 --metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage \
