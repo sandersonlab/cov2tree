7a17cd2ba5a1850ce5ac2335532c9c4e11efd6e1
Theo Sanderson <theo@sndrsn.co.uk>
2022-05-21 13:20:00 +0100

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,23 @@
+
+mkdir working
+cd working
+# Replace MYSECRETKEY with the contents of the key
+sed  "s/MYSECRETKEY/$MYKEY/g" ../s3cfg > s3cfg
+s3cmd put -P ../README.md -c s3cfg s3://cov2tree/README.md
+wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.metadata.tsv.gz
+wget -q https://raw.githubusercontent.com/theosanderson/taxonium/master/taxoniumtools/test_data/hu1.gb
+wget -q https://hgwdev.gi.ucsc.edu/~angie/UShER_SARS-CoV-2/public-latest.all.masked.pb.gz
+wget -q https://raw.githubusercontent.com/theosanderson/treeShears/master/treeshears.py
+
+sudo pip3 install taxoniumtools chronumental
+
+gunzip public-latest.all.masked.pb.gz
+python3 treeshears.py -i public-latest.all.masked.pb -o public-latest.all.masked.sheared.pb -T 5000
+
+
+usher_to_taxonium --input public-latest.all.masked.sheared.pb --output ./public.jsonl.gz \
+--metadata public-latest.metadata.tsv.gz --genbank hu1.gb --columns genbank_accession,country,date,pangolin_lineage \
+--overlay_html ../ack.html --title Cov2Tree --remove_after_pipe --chronumental_date_output=chron_dates.tsv --chronumental_tree_output=chron_tree.nwk \
+--clade_types=nextstrain,pango --shear
+
+s3cmd put -P public.jsonl.gz -c s3cfg s3://cov2tree/latest_public_custom.jsonl.gz 
